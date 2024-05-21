b5be15d6f9b51c0da168b825c5ccee3f3a5568e8
TalKoren11 <kotal@post.bgu.ac.il>
2024-05-21 21:05:21 +0300

    added basic skeleton for purchesCart in ShoppingCart.

--- src/main/java/Domain/ShoppingCart.java
+++ src/main/java/Domain/ShoppingCart.java
@@ -2,11 +2,19 @@ package Domain;
 
 import java.util.List;
 
+import Domain.ExternalServices.PaymentService.PaymentMethod;
+import Domain.ExternalServices.SupplyService.SupplyMethod;
+
 public class ShoppingCart {
     private List<ShoppingBasket> _shoppingBaskets;
 
     public ShoppingCart(List<ShoppingBasket> shoppingBaskets) {
         _shoppingBaskets = shoppingBaskets;
+    }
+
+    public String purchesCart(User buyer, PaymentMethod paymentMethod, SupplyMethod shippingMethod, List<Integer> busketsToBuy) {
+            
+        return "";
     }
 
     public String toString() {
