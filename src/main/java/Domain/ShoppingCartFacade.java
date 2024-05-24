99e4e4e2050fc3cd49edd0871afb195b0ff2e683
amit-levints-bgu <levints@post.bgu.ac.il>
2024-05-24 21:33:14 +0300

    refactor

--- src/main/java/Domain/ShoppingCartFacade.java
+++ src/main/java/Domain/ShoppingCartFacade.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ShoppingCartFacade {
-    Map<String, ShoppingCart> _guestsCarts; // <GuestToken, ShoppingCart>
+    Map<String, ShoppingCart> _guestsCarts; // <guestID, ShoppingCart>
     Map<String, ShoppingCart> _usersCarts; // <Username, ShoppingCart>
     private static final Logger logger = Logger.getLogger(ShoppingCartFacade.class.getName());
 
@@ -22,9 +22,9 @@ public class ShoppingCartFacade {
     /*
      * Add a cart for a guest by token.
      */
-    public void addCartForGuest(String guestToken) {
+    public void addCartForGuest(String guestID) {
         ShoppingCart cart = new ShoppingCart();
-        _guestsCarts.put(guestToken, cart);
+        _guestsCarts.put(guestID, cart);
     }
 
     /*
@@ -32,25 +32,25 @@ public class ShoppingCartFacade {
      * This methos called only when a guest user register to the system.
      * The cart of the guest user will be added to the user's cart and then will be deleted in the guests carts.
      */
-    public void addCartForUser(String guestToken, String username) {
-        _usersCarts.put(username, _guestsCarts.remove(guestToken));
+    public void addCartForUser(String guestID, String username) {
+        _usersCarts.put(username, _guestsCarts.remove(guestID));
     }
 
     /*
      * Remove a cart from a guest user by token.
      * This method called when a guest user leave the system.
      */
-    public void removeCartForGuest(String guestToken) {
-        _guestsCarts.remove(guestToken);
+    public void removeCartForGuest(String guestID) {
+        _guestsCarts.remove(guestID);
     }
 
-    public void purchaseCartGuest(String guestToken,  String cardNumber, String address) {
+    public void purchaseCartGuest(String guestID,  String cardNumber, String address) {
         ArrayList<Integer> allBaskets = new ArrayList<Integer>();
 
-        for (int i = 0; i < _guestsCarts.get(guestToken).getCartSize(); i++) 
+        for (int i = 0; i < _guestsCarts.get(guestID).getCartSize(); i++) 
             allBaskets.add(i + 1);
         logger.log(Level.INFO, "Start purchasing cart for guest.");
-        _guestsCarts.get(guestToken).purchaseCart(allBaskets, cardNumber, address);
+        _guestsCarts.get(guestID).purchaseCart(allBaskets, cardNumber, address);
     }
 
     public void purchaseCartUser(String username, List<Integer> busketsToBuy, String cardNumber, String address) {
