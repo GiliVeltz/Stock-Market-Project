6afefa6a1efaf9eeea0540902d2ee4cad711cf4c
d4nieldev <72974081+d4nieldev@users.noreply.github.com>
2024-06-02 10:23:37 +0300

    server close shop endpoint (#289)
    Co-authored-by: inbarbc <102466931+inbarbc@users.noreply.github.com>

--- src/main/java/Server/ShopController.java
+++ src/main/java/Server/ShopController.java
@@ -1,10 +1,12 @@
 package Server;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import Dtos.ShopDto;
@@ -27,4 +29,8 @@ public class ShopController {
 
     }
 
+    @GetMapping("/closeShop")
+    public Response closeShop(@RequestHeader("Authorization") String token, @RequestParam Integer shopId) {
+        return _shopService.closeShop(token, shopId);
+    }
 }
