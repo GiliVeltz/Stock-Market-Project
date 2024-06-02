40840108818978d00c375c260bf1db87e87ac2f9
danielohbgu <danielohayon2019@gmail.com>
2024-06-02 09:21:21 +0300

    updated controller to call service with the shop dto

--- src/main/java/Server/ShopController.java
+++ src/main/java/Server/ShopController.java
@@ -1,13 +1,18 @@
 package Server;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import Dtos.ShopDto;
+import ServiceLayer.Response;
 import ServiceLayer.ShopService;
 
 @RestController
-@RequestMapping(path = "/api/user")
+@RequestMapping(path = "/api/shop")
 public class ShopController {
     private final ShopService _shopService;
 
@@ -16,5 +21,10 @@ public class ShopController {
         this._shopService = shopService;
     }
 
-    
+    @PostMapping("/openNewShop")
+    public Response openNewShop(@RequestHeader("Authorization") String token, @RequestBody ShopDto shopDto) {
+        return _shopService.openNewShop(token, shopDto);
+
+    }
+
 }
