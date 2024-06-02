45ec0c2641323dd9117b023e5c290ade6bb02739
danielohbgu <danielohayon2019@gmail.com>
2024-06-02 09:28:30 +0300

    added the endpoint

--- src/main/java/Server/ShopController.java
+++ src/main/java/Server/ShopController.java
@@ -1,10 +1,12 @@
 package Server;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import Dtos.ShopDto;
@@ -25,6 +27,11 @@ public class ShopController {
     public Response openNewShop(@RequestHeader("Authorization") String token, @RequestBody ShopDto shopDto) {
         return _shopService.openNewShop(token, shopDto);
 
+    }
+
+    @GetMapping("/closeShop")
+    public Response closeShop(@RequestHeader("Authorization") String token, @RequestParam Integer shopId) {
+        return _shopService.closeShop(token, shopId);
     }
 
 }
