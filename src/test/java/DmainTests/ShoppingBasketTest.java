68e22fe200ea11e5595e1f53dea8ba537a3857e3
TalKoren11 <kotal@post.bgu.ac.il>
2024-06-05 01:53:15 +0300

    2 more tests for purchase basket.

--- src/test/java/DmainTests/ShoppingBasketTest.java
+++ src/test/java/DmainTests/ShoppingBasketTest.java
@@ -36,15 +36,14 @@ public class ShoppingBasketTest {
         Date date = new Date();
         date.setTime(0);
         User buyer = new User("username1", "password1", "email1", date);
-        User owner = new User("username2", "password2", "email2", date);
-        Shop shop = new Shop(1, "username2", "bank1", "address1");
+        Shop shop = new Shop(1, "ownerUsername", "bank1", "address1");
         ShoppingBasket shoppingBasket = new ShoppingBasket(shop);
         Product product = new Product("product1", Category.ELECTRONICS, 100.0);
         product.updateProductQuantity(3);
-        shop.addProductToShop("username2", product);
+        shop.addProductToShop("ownerUsername", product);
         Product product2 = new Product("product2", Category.ELECTRONICS, 100.0);
         product2.updateProductQuantity(10);
-        shop.addProductToShop("username2", product2);
+        shop.addProductToShop("ownerUsername", product2);
         shoppingBasket.addProductToShoppingBasket(buyer, product.getProductId());
         shoppingBasket.addProductToShoppingBasket(buyer, product.getProductId());
         shoppingBasket.addProductToShoppingBasket(buyer, product.getProductId());
@@ -66,15 +65,14 @@ public class ShoppingBasketTest {
         Date date = new Date();
         date.setTime(0);
         User buyer = new User("username1", "password1", "email1", date);
-        User owner = new User("username2", "password2", "email2", date);
-        Shop shop = new Shop(1, "username2", "bank1", "address1");
+        Shop shop = new Shop(1, "ownerUsername", "bank1", "address1");
         ShoppingBasket shoppingBasket = new ShoppingBasket(shop);
         Product product = new Product("product1", Category.ELECTRONICS, 100.0);
         product.updateProductQuantity(3);
-        shop.addProductToShop("username2", product);
+        shop.addProductToShop("ownerUsername", product);
         Product product2 = new Product("product2", Category.ELECTRONICS, 100.0);
-        product2.updateProductQuantity(10);
-        shop.addProductToShop("username2", product2);
+        product2.updateProductQuantity(0);
+        shop.addProductToShop("ownerUsername", product2);
         shoppingBasket.addProductToShoppingBasket(buyer, product.getProductId());
         shoppingBasket.addProductToShoppingBasket(buyer, product.getProductId());
         shoppingBasket.addProductToShoppingBasket(buyer, product.getProductId());
@@ -84,9 +82,40 @@ public class ShoppingBasketTest {
         boolean result = shoppingBasket.purchaseBasket();
         
         // Assert
-        assertEquals(result, true);
-        assertEquals(product.getProductQuantity(), 0);
-        assertEquals(product2.getProductQuantity(), 9);
+        assertEquals(result, false);
+        assertEquals(product.getProductQuantity(), 3);
+        assertEquals(product2.getProductQuantity(), 0);
+    }
+
+    @Test
+    public void testPurchaseBasket_whenBasketMeetsShopPolicyAndFirstProductInStockSecondSomeInStock_thenReturnedFalseAndRestockProducts() throws ProdcutPolicyException,
+     ShopPolicyException, ProductAlreadyExistsException, PermissionException, StockMarketException {
+        // Arrange
+        Date date = new Date();
+        date.setTime(0);
+        User buyer = new User("username1", "password1", "email1", date);
+        Shop shop = new Shop(1, "ownerUsername", "bank1", "address1");
+        ShoppingBasket shoppingBasket = new ShoppingBasket(shop);
+        Product product = new Product("product1", Category.ELECTRONICS, 100.0);
+        product.updateProductQuantity(3);
+        shop.addProductToShop("ownerUsername", product);
+        Product product2 = new Product("product2", Category.ELECTRONICS, 100.0);
+        product2.updateProductQuantity(1);
+        shop.addProductToShop("ownerUsername", product2);
+        shoppingBasket.addProductToShoppingBasket(buyer, product.getProductId());
+        shoppingBasket.addProductToShoppingBasket(buyer, product.getProductId());
+        shoppingBasket.addProductToShoppingBasket(buyer, product.getProductId());
+        shoppingBasket.addProductToShoppingBasket(buyer, product2.getProductId());
+        shoppingBasket.addProductToShoppingBasket(buyer, product2.getProductId());
+        shoppingBasket.addProductToShoppingBasket(buyer, product2.getProductId());
+
+        // Act
+        boolean result = shoppingBasket.purchaseBasket();
+        
+        // Assert
+        assertEquals(result, false);
+        assertEquals(product.getProductQuantity(), 3);
+        assertEquals(product2.getProductQuantity(), 1);
     }
     
 }
