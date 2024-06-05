bc466fdc11c4d82dcd494098dab96adf1bd9f0bf
TalKoren11 <kotal@post.bgu.ac.il>
2024-06-05 16:00:25 +0300

    Revert "added PurchaseProduct when out of stock test."
    This reverts commit 08396ba1bfbfdf8619cdd4e16514857f30adb872.

--- src/test/java/DmainTests/ProductTest.java
+++ src/test/java/DmainTests/ProductTest.java
@@ -1,7 +1,6 @@
 package DmainTests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
