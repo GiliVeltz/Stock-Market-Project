e4051e7ed79969c47379bd5851005ab05574c0ef
orsaada <saadao@post.bgu.ac.il>
2024-06-07 23:46:10 +0300

    product test

--- src/test/java/DmainTests/ProductTests.java
+++ src/test/java/DmainTests/ProductTests.java
@@ -1,7 +1,9 @@
 package DmainTests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.AfterEach;
@@ -92,6 +94,33 @@ public class ProductTests {
         assertThrows(StockMarketException.class, () -> {product.addProductRating(0);});        
         assertThrows(StockMarketException.class, () -> {product.addProductRating(-5);});        
 
+    }
+
+    
+    @Test
+    public void testIsPriceInRange_whenPriceInRange_thenSuccess()  {
+        // Arrange - Create a new Product object.
+        Product product = new Product(1, "product1", Category.ELECTRONICS, 100.0);
+        product.updateProductQuantity(10);
+        product.setPrice(55);
+        
+        boolean result = product.isPriceInRange(10, 90);
+        
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsPriceInRange_whenPriceOutOfRange_thenFalse()  {
+        // Arrange - Create a new Product object.
+        Product product = new Product(1, "product1", Category.ELECTRONICS, 100.0);
+        product.updateProductQuantity(10);
+        product.setPrice(55);
+        
+        boolean result = product.isPriceInRange(10, 20);
+        
+        // Assert
+        assertFalse(result);
     }
 
 }
