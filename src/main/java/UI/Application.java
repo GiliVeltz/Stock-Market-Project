096961041f1c6ffd434a9952a5c291fe3b7e39e6
Vladislav Shembel <104021621+vladik1998@users.noreply.github.com>
2024-06-08 23:13:01 +0300

    343 UI connect to domain (#379)

--- src/main/java/UI/Application.java
+++ src/main/java/UI/Application.java
@@ -1,9 +1,21 @@
 package UI;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.UIInitEvent;
+import com.vaadin.flow.server.UIInitListener;
 import com.vaadin.flow.theme.Theme;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * The entry point of the Spring Boot application.
@@ -14,9 +26,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @Theme(value = "flowcrmtutorial")
-public class Application implements AppShellConfigurator {
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
+
+    //TODO: Maybe use the same port for both server and client
     public static void main(String[] args) {
+        System.setProperty("server.port", "8081");
         SpringApplication.run(Application.class, args);
     }
 
