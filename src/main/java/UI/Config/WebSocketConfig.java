594086e40e4c021b024eb8fd67d03a070ce4e5c7
BacharMetar <metar@post.bgu.ac.il>
2024-06-08 15:56:24 +0300

    first step on web socket

---
+++ src/main/java/UI/Config/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package UI.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+
+//A configuration class for setting up WebSocket communication in a Java application. 
+
+@Configuration 
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    // Configure the message broker to enable a simple broker for "/topic" destinations
+    // and set the application destination prefix to "/app"
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    // Register the "/ws" endpoint and enable SockJS fallback options
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+}
