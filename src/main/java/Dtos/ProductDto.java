d5577725ff113f5039f7c521fde2beca09d573e5
TalKoren11 <102467459+TalKoren11@users.noreply.github.com>
2024-06-16 22:15:07 +0300

    A lot of tests for purchase cart. (#457)

--- src/main/java/Dtos/ProductDto.java
+++ src/main/java/Dtos/ProductDto.java
@@ -8,18 +8,21 @@ public class ProductDto {
     public String _productName;
     public Category _category;
     public double _price;
+    public int _productQuantity;
 
     // Constructor
-    public ProductDto(String productName, Category category, double price) {
+    public ProductDto(String productName, Category category, double price, int productQuantity) {
         this._productName = productName;
         this._category = category;
         this._price = price;
+        this._productQuantity = productQuantity;
     }
 
     public ProductDto(Product product) {
         this._productName = product.getProductName();
         this._category = product.getCategory();
         this._price = product.getPrice();
+        this._productQuantity = product.getProductQuantity();
     }
 
 }
