450c6eb81328d9807d1c3f0720faf1eaeb0fe9c5
Daniel Ohayon <danielohayon2019@gmail.com>
2024-06-30 14:40:02 +0300

    added docs for all rules

--- src/main/java/Domain/Rules/TimeRangeInDayRule.java
+++ src/main/java/Domain/Rules/TimeRangeInDayRule.java
@@ -2,10 +2,22 @@ package Domain.Rules;
 
 import java.util.Calendar;
 
+/**
+ * Represents a rule that checks if the current time falls within a specified time range in a day.
+ * @param <T> the type of object - ignored by the rule
+ */
 public class TimeRangeInDayRule<T> implements Rule<T> {
+    
     int _startHours, _startMinutes;
     int _endHours, _endMinutes;
 
+    /**
+     * Constructs a TimeRangeInDayRule object with the specified start and end time.
+     * @param startHours the starting hour of the time range
+     * @param startMinutes the starting minute of the time range
+     * @param endHours the ending hour of the time range
+     * @param endMinutes the ending minute of the time range
+     */
     public TimeRangeInDayRule(int startHours, int startMinutes, int endHours, int endMinutes) {
         _startHours = startHours;
         _startMinutes = startMinutes;
