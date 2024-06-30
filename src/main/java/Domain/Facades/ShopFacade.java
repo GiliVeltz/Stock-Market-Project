1305a6245057b99aa05f4e4bedfce928ae01d307
Vladislav Shembel <104021621+vladik1998@users.noreply.github.com>
2024-06-30 23:23:09 +0300

    add modify permissions functiuonality (#588)

--- src/main/java/Domain/Facades/ShopFacade.java
+++ src/main/java/Domain/Facades/ShopFacade.java
@@ -54,13 +54,21 @@ public class ShopFacade {
     public ShopFacade(InterfaceShopRepository shopRepository) {
         _shopRepository = shopRepository;
         _userFacade = UserFacade.getUserFacade();
+
+        //For testing UI
+        // try {
+        //     initUI();
+        // }
+        // catch (StockMarketException e) {
+        //     e.printStackTrace();
+        // }
     }
 
-        public ShopFacade() {
+    public ShopFacade() {
         _shopRepository = new MemoryShopRepository(new ArrayList<>());
         _userFacade = UserFacade.getUserFacade();
 
-        // For testing UI
+        //For testing UI
         // try {
         //     initUI();
         // }
@@ -793,20 +801,20 @@ public class ShopFacade {
         shop.addKeywordsToProduct(username, productId, keywords);
     }
 
-    // // function to initilaize data for UI testing
-    // public void initUI() throws StockMarketException {
-    //     // Shop shop = new Shop(10, "shopUITest", "Tal", "bankUITest", "addressUITest");
-    //     // _shopRepository.addShop(shop);
-    //     // Product product = new Product(10, "productUITest", Category.ELECTRONICS, 100.0);
-    //     // product.updateProductQuantity(10);
-    //     // shop.addProductToShop("Tal", product);
+    // function to initilaize data for UI testing
+    public void initUI() throws StockMarketException {
+        // Shop shop = new Shop(10, "shopUITest", "Tal", "bankUITest", "addressUITest");
+        // _shopRepository.addShop(shop);
+        // Product product = new Product(10, "productUITest", Category.ELECTRONICS, 100.0);
+        // product.updateProductQuantity(10);
+        // shop.addProductToShop("Tal", product);
 
-    //     openNewShop("tal", new ShopDto("shopUITest", "bankUITest", "addressUITest"));
-    //     openNewShop("tal", new ShopDto("shopUITest2", "bankUITest2", "addressUITest2"));
-    //     addProductToShop(0, new ProductDto("productUITest", Category.ELECTRONICS, 100.0, 10), "tal");
-    //     addProductToShop(1, new ProductDto("productUITest2", Category.ELECTRONICS, 207.5, 10), "tal");
-    //     addProductToShop(1, new ProductDto("productUITest3", Category.ELECTRONICS, 100.0, 10), "tal");
-    // }
+        openNewShop("tal", new ShopDto("shopUITest", "bankUITest", "addressUITest"));
+        openNewShop("tal", new ShopDto("shopUITest2", "bankUITest2", "addressUITest2"));
+        addProductToShop(0, new ProductDto("productUITest", Category.ELECTRONICS, 100.0, 10), "tal");
+        addProductToShop(1, new ProductDto("productUITest2", Category.ELECTRONICS, 207.5, 10), "tal");
+        addProductToShop(1, new ProductDto("productUITest3", Category.ELECTRONICS, 100.0, 10), "tal");
+    }
 
     // this function is responsible for getting all the shop managers
     @Transactional
@@ -933,4 +941,21 @@ public class ShopFacade {
         }
         shop.removeDiscount(discountDto.id);
     }
+
+    // Update the permissins of manager in shop.
+    @Transactional
+    public void updatePermissions(String username, Integer shopId, String managerUsername, Set<String> permissions)
+        throws StockMarketException {
+        Shop shop = getShopByShopId(shopId);
+        if (shop == null) {
+            throw new StockMarketException(String.format("Shop ID: %d doesn't exist.", shopId));
+        }
+        // Here we create a set of permissions from the strings.
+        Set<Permission> permissionsSet = permissions.stream()
+                .map(permissionString -> Permission.valueOf(permissionString.toUpperCase()))
+                .collect(Collectors.toSet());
+        shop.modifyPermissions(username, managerUsername, permissionsSet);
+    }
+
+
 }
