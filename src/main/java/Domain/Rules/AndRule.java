c1233ca6cb372135e612b170a04ff8613d35961c
Daniel Ohayon <danielohayon2019@gmail.com>
2024-06-30 14:32:44 +0300

    added AndRule

---
+++ src/main/java/Domain/Rules/AndRule.java
@@ -0,0 +1,16 @@
+package Domain.Rules;
+
+public class AndRule<T> implements Rule<T> {
+    Rule<T> _rule1;
+    Rule<T> _rule2;
+
+    public AndRule(Rule<T> rule1, Rule<T> rule2) {
+        _rule1 = rule1;
+        _rule2 = rule2;
+    }
+
+    @Override
+    public boolean predicate(T obj) {
+        return _rule1.predicate(obj) && _rule2.predicate(obj);
+    }
+}
