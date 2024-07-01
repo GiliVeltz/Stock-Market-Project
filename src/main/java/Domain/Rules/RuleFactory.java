90a6b7397ad21b883f7455b86e2deae3930a9823
Daniel Ohayon <danielohayon2019@gmail.com>
2024-07-01 09:46:08 +0300

    moved time in day rule to generic rules

--- src/main/java/Domain/Rules/RuleFactory.java
+++ src/main/java/Domain/Rules/RuleFactory.java
@@ -2,6 +2,7 @@ package Domain.Rules;
 
 import Domain.ShoppingBasket;
 import Dtos.Rules.AllItemsRuleDto;
+import Dtos.Rules.GenericRuleDto;
 import Dtos.Rules.MinBasketPriceRuleDto;
 import Dtos.Rules.MinProductAmountRuleDto;
 import Dtos.Rules.ShoppingBasketRuleDto;
@@ -9,14 +10,14 @@ import Dtos.Rules.TimeRangeInDayRuleDto;
 
 public class RuleFactory {
     public static Rule<ShoppingBasket> createShoppingBasketRule(ShoppingBasketRuleDto dto) {
+        if (dto instanceof GenericRuleDto)
+            return createGenericRule((GenericRuleDto) dto);
         if (dto instanceof MinBasketPriceRuleDto)
             return createShoppingBasketRule((MinBasketPriceRuleDto) dto);
         if (dto instanceof AllItemsRuleDto)
             return createShoppingBasketRule((AllItemsRuleDto) dto);
         if (dto instanceof MinProductAmountRuleDto)
             return createShoppingBasketRule((MinProductAmountRuleDto) dto);
-        if (dto instanceof TimeRangeInDayRuleDto)
-            return createShoppingBasketRule((TimeRangeInDayRuleDto) dto);
         
         throw new IllegalArgumentException("Unknown rule type");
     }
@@ -32,8 +33,16 @@ public class RuleFactory {
     private static Rule<ShoppingBasket> createShoppingBasketRule(MinProductAmountRuleDto dto) {
         return new MinProductAmountRule(dto.productId, dto.minAmount);
     }
+    
 
-    private static Rule<ShoppingBasket> createShoppingBasketRule(TimeRangeInDayRuleDto dto) {
-        return new TimeRangeInDayRule<ShoppingBasket>(dto.startHour, dto.startMinutes, dto.endHour, dto.endMinutes);
+    private static <T> Rule<T> createGenericRule(GenericRuleDto dto) {
+        if (dto instanceof TimeRangeInDayRuleDto)
+            return createGenericRule((TimeRangeInDayRuleDto) dto);
+
+        throw new IllegalArgumentException("Unknown rule type");
+    }
+
+    private static <T> Rule<T> createGenericRule(TimeRangeInDayRuleDto dto) {
+        return new TimeRangeInDayRule<T>(dto.startHour, dto.startMinutes, dto.endHour, dto.endMinutes);
     }
 }
