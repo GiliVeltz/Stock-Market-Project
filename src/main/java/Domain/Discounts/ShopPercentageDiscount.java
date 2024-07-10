d41bba02f2eb5aafae0957029e8992b44304020c
TalKoren11 <kotal@post.bgu.ac.il>
2024-07-11 01:59:18 +0300

    fixed bugs

--- src/main/java/Domain/Discounts/ShopPercentageDiscount.java
+++ src/main/java/Domain/Discounts/ShopPercentageDiscount.java
@@ -1,6 +1,7 @@
 package Domain.Discounts;
 
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
@@ -44,7 +45,8 @@ public class ShopPercentageDiscount extends BaseDiscount {
     protected void applyDiscountLogic(ShoppingBasket basket) {
         if (!_rule.predicate(basket))
             return;
-        for (int product_id : basket.getProductIdList()) {
+        
+        for (int product_id : new HashSet<>(basket.getProductIdList())) {
             SortedMap<Double, Integer> newpriceToAmount = new TreeMap<>();
             SortedMap<Double, Integer> priceToAmount = basket.getProductPriceToAmount(product_id);
             for (Map.Entry<Double, Integer> entry : priceToAmount.entrySet()) {
