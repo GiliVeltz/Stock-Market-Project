30b0d33a7dddfe971847bee1d94d41c18e7a6629
BacharMetar <metar@post.bgu.ac.il>
2024-07-17 22:47:30 +0300

    merge commit

--- src/main/java/Domain/Entities/User.java
+++ src/main/java/Domain/Entities/User.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import Dtos.UserDto;
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -20,6 +21,7 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import jakarta.persistence.Transient;
